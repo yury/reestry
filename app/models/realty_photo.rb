628723d0583779101f2bdf9ebe50f5cef9ccb61d
Nick <nick@grayinvader.(none)>
2009-07-24 19:58:05 +0400

    fix photos

--- app/models/realty_photo.rb
+++ app/models/realty_photo.rb
@@ -1,11 +1,12 @@
 class RealtyPhoto < ActiveRecord::Base
   belongs_to :realty
   
-  has_attached_file :photo, :styles => { :small => "120x90>", :original => "468x351>" },
-                    :url  => "/assets/products/:id/:style/:basename.:extension",
-                    :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
+  has_attached_file :photo, :styles => { :small => "120x90#", :original => "468x351>" },
+                    :convert_options => { :small => '-strip -quality 75', :original => '-strip'},
+                    :url  => "/assets/images/:id/:style/:basename.:extension",
+                    :path => ":rails_root/public/assets/images/:id/:style/:basename.:extension"
 
-  validates_attachment_presence :photo, :message => "Необходимо указать фотографию"
-  validates_attachment_size :photo, :less_than => 5.megabytes, :message => "Размер фотографии не может превышать 5 Мб"
-  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png', 'image/gif'], :message => "Поддерживаются только картинки с расширением .jpeg, .png и .gif"
+  validates_attachment_presence :photo#, :options => {:message => "Необходимо указать фотографию"}
+  validates_attachment_size :photo, :less_than => 5.megabytes#, :message => "Размер фотографии не может превышать 5 Мб"
+  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png', 'image/gif']#, :message => "Поддерживаются только картинки с расширением .jpeg, .png и .gif"
 end
--- config/environment.rb
+++ config/environment.rb
@@ -62,7 +62,7 @@ Rails::Initializer.run do |config|
   # This is necessary if your schema can't be completely dumped by the schema dumper,
   # like if you have constraints or database-specific column types
   # config.active_record.schema_format = :sql
-
+  config.whiny_nils = true
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
 end
