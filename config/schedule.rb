84daa5bba59012a3c2beb1c984968ea70e3e42b8
yury <yury.korolev@gmail.com>
2010-03-26 18:45:01 +0300

    schedule fix

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,6 +4,10 @@ set :environment, :production
 env "LANG", "en_US.UTF-8"
 env "TZ", "Europe/Moscow"
 
+def run cmd
+  command "#{@rails_root}/bin/run #{cmd}"
+end
+
 every 1.days, :at => '2:01am' do
   run "rake parse:start"
 end
