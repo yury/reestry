134759937d59b950228c994053486d9abd7bad67
yury <yury.korolev@gmail.com>
2010-05-25 13:27:54 +0400

    Migrate reestry to rails 2.3.8

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source :gemcutter
 
-gem "rails", "~> 2.3.7"
+gem "rails", "~> 2.3.8"
 gem "unichars"
 gem "erubis"
 gem 'whenever'
--- config/initializers/erubis.rb
+++ config/initializers/erubis.rb
@@ -22,39 +22,6 @@ class FixedErubis < ::Erubis::Eruby
   end
 end
 
-module AvPatch
-  # Rails version of with_output_buffer uses '' as the default buf
-  def with_output_buffer(buf = '') #:nodoc:
-    super(buf)
-  end
-end
-
-ActionView::Base.send :include, AvPatch
-
-module ActionView
-  class Base
-    def self.xss_safe?
-      true
-    end
-  end
-end
-
-# fix translation helpers (bug in 2.3.7 will be fixed in 2.3.8)
-ActionView::Helpers::TranslationHelper.module_eval do
-  def translate(key, options = {})
-    options[:raise] = true
-    translation = I18n.translate(scope_key_by_partial(key), options)
-    translation
-  rescue I18n::MissingTranslationData => e
-    keys = I18n.send(:normalize_translation_keys, e.locale, e.key, e.options[:scope])
-    content_tag('span', keys.join(', '), :class => 'translation_missing')
-  end
-
-  alias :t :translate
-end
-
-
-
 Erubis::Helpers::RailsHelper.engine_class = FixedErubis
 #Erubis::Helpers::RailsHelper.init_properties = {}
 Erubis::Helpers::RailsHelper.show_src = false       # set true for debugging
