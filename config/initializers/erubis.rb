f67e133269766a3b7a34afa074763c84a82ae10a
yury <yury.korolev@gmail.com>
2010-06-06 15:49:59 +0400

    Fix reestry

--- config/initializers/erubis.rb
+++ config/initializers/erubis.rb
@@ -26,3 +26,16 @@ Erubis::Helpers::RailsHelper.engine_class = FixedErubis
 #Erubis::Helpers::RailsHelper.init_properties = {}
 Erubis::Helpers::RailsHelper.show_src = false       # set true for debugging
 Erubis::Helpers::RailsHelper.preprocessing = false   # set true to enable preprocessing
+
+class ActiveSupport::BufferedLogger
+  def add(severity, message = nil, progname = nil, &block)
+    return if @level > severity
+    message = (message || (block && block.call) || progname).to_s
+    # If a newline is necessary then create a new message ending with a newline.
+    # Ensures that the original message is not mutated.
+    message = "#{message}\n" unless message[-1] == ?\n
+    buffer << message.force_encoding("utf-8") if "".respond_to?(:encoding)
+    auto_flush
+    message
+  end
+end
